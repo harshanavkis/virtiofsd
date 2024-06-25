harshanavkis/virtiofsd

commit 8b459a064de725438bee5ba2367bc32bd0963976

Add '--shared-dir' as required if '-o' is missing
If virtiofsd is run without any option an error is shown
with required arguments. Since, we accept both '--shared-dir'
and '-o source=' we cannot define '--shared-dir' as required.

Let's set it as required unless the '-o' option is provided.
This is not perfect, since it will not show '--shared-dir'
as required if any  '-o' is provided, not only '-o source',
but I think is enough because '-o' is deprecated, and it will
be removed eventually.

Fixes: https://issues.redhat.com/browse/RHEL-7348
Signed-off-by: German Maglione <gmaglione@redhat.com>

src/main.rs
@@ -577,7 +577,7 @@ fn parse_tag(tag: &str) -> Result<String> {
 )]
 struct Opt {
     /// Shared directory path
-    #[arg(long)]
+    #[arg(long, required_unless_present("compat_options"))]
     shared_dir: Option<String>,
 
     /// The tag that the virtio device advertises

commit 9d1be9530fff1df342bfae7a0d9f80c5d29a2737

Do not require --shared-dir to print capabilities
An earlier commit added an extra check to provide a more helpful error
message, but it also meant bare 'virtiofsd --print-capabilities' stopped
working.

Fixes: 8b459a064de725438bee5ba2367bc32bd0963976

src/main.rs
@@ -712,7 +712,7 @@ fn parse_tag(tag: &str) -> Result<String> {
 )]
 struct Opt {
     /// Shared directory path
-    #[arg(long, required_unless_present("compat_options"))]
+    #[arg(long, required_unless_present_any = &["compat_options", "print_capabilities"])]
     shared_dir: Option<String>,
 
     /// The tag that the virtio device advertises