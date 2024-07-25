4309a3d38006395e797b86aac5e09c121c3c72cc
Hanna Czenczek <hreitz@redhat.com>
2024-07-25 16:56:46 +0200

    Make migration errors visible
    Error objects returned from vhost-user migration methods
    (SET_DEVICE_STATE_FD, CHECK_DEVICE_STATE) are currently lost: We return
    them to the vhost-user-backend crate, but eventually they are just
    dropped there.
    
    So log them before returning them.
    
    Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -578,6 +578,33 @@ impl<F: FileSystem + SerializableFileSystem + Send + Sync + 'static> VhostUserBa
         phase: VhostTransferStatePhase,
         file: File,
     ) -> io::Result<Option<File>> {
+        // Our caller (vhost-user-backend crate) pretty much ignores error objects we return (only
+        // cares whether we succeed or not), so log errors here
+        if let Err(err) = self.do_set_device_state_fd(direction, phase, file) {
+            error!("Failed to initiate state (de-)serialization: {err}");
+            return Err(err);
+        }
+        Ok(None)
+    }
+
+    fn check_device_state(&self) -> io::Result<()> {
+        // Our caller (vhost-user-backend crate) pretty much ignores error objects we return (only
+        // cares whether we succeed or not), so log errors here
+        if let Err(err) = self.do_check_device_state() {
+            error!("Failed to conclude migration: {err}");
+            return Err(err);
+        }
+        Ok(())
+    }
+}
+
+impl<F: FileSystem + SerializableFileSystem + Send + Sync + 'static> VhostUserFsBackend<F> {
+    fn do_set_device_state_fd(
+        &self,
+        direction: VhostTransferStateDirection,
+        phase: VhostTransferStatePhase,
+        file: File,
+    ) -> io::Result<()> {
         if phase != VhostTransferStatePhase::STOPPED {
             return Err(io::Error::new(
                 io::ErrorKind::Unsupported,
@@ -621,10 +648,10 @@ impl<F: FileSystem + SerializableFileSystem + Send + Sync + 'static> VhostUserBa
 
         *self.migration_thread.lock().unwrap() = Some(join_handle);
 
-        Ok(None)
+        Ok(())
     }
 
-    fn check_device_state(&self) -> io::Result<()> {
+    fn do_check_device_state(&self) -> io::Result<()> {
         let result = if let Some(migration_thread) = self.migration_thread.lock().unwrap().take() {
             // `Result::flatten()` is not stable yet, so no `.join().map_err(...).flatten()`
             match migration_thread.join() {
