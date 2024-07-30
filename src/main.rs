harshanavkis/virtiofsd

commit 4309a3d38006395e797b86aac5e09c121c3c72cc

Make migration errors visible
Error objects returned from vhost-user migration methods
(SET_DEVICE_STATE_FD, CHECK_DEVICE_STATE) are currently lost: We return
them to the vhost-user-backend crate, but eventually they are just
dropped there.

So log them before returning them.

Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

src/main.rs
@@ -578,6 +578,33 @@ impl<F: FileSystem + SerializableFileSystem + Send + Sync + 'static> VhostUserBa
         phase: VhostTransferStatePhase,
         file: File,
     ) -> io::Result<Option<File>> {
+        // Our caller (vhost-user-backend crate) pretty much ignores error objects we return (only
+        // cares whether we succeed or not), so log errors here
+        if let Err(err) = self.do_set_device_state_fd(direction, phase, file) {
+            error!("Failed to initiate state (de-)serialization: {err}");
+            return Err(err);
+        }
+        Ok(None)
+    }
+
+    fn check_device_state(&self) -> io::Result<()> {
+        // Our caller (vhost-user-backend crate) pretty much ignores error objects we return (only
+        // cares whether we succeed or not), so log errors here
+        if let Err(err) = self.do_check_device_state() {
+            error!("Failed to conclude migration: {err}");
+            return Err(err);
+        }
+        Ok(())
+    }
+}
+
+impl<F: FileSystem + SerializableFileSystem + Send + Sync + 'static> VhostUserFsBackend<F> {
+    fn do_set_device_state_fd(
+        &self,
+        direction: VhostTransferStateDirection,
+        phase: VhostTransferStatePhase,
+        file: File,
+    ) -> io::Result<()> {
         if phase != VhostTransferStatePhase::STOPPED {
             return Err(io::Error::new(
                 io::ErrorKind::Unsupported,
@@ -621,10 +648,10 @@ impl<F: FileSystem + SerializableFileSystem + Send + Sync + 'static> VhostUserBa
 
         *self.migration_thread.lock().unwrap() = Some(join_handle);
 
-        Ok(None)
+        Ok(())
     }
 
-    fn check_device_state(&self) -> io::Result<()> {
+    fn do_check_device_state(&self) -> io::Result<()> {
         let result = if let Some(migration_thread) = self.migration_thread.lock().unwrap().take() {
             // `Result::flatten()` is not stable yet, so no `.join().map_err(...).flatten()`
             match migration_thread.join() {

commit 335ac5ab4f29ac5350353c12b53c4ad905fa61c7

Simplify VhostUserFsBackend::do_check_device_state()
As of 4309a3d38006395e797b86aac5e09c121c3c72cc, which introduced
do_check_device_state(), errors it returns are always printed to the
log.  It doesn’t need to do that itself.

Less formally: 4309a3d38006395e797b86aac5e09c121c3c72cc wanted to fix
the problem that errors in set_device_state_fd() aren’t printed.  It
assumed the same to be true about check_device_state(), but it turns out
that function already took care to print errors.  So it wasn’t actually
necessary to fix it.

Anyway, it’s reasonable to use the same pattern for both
set_device_state_fd() and check_device_state() (i.e. to implement their
functionality in do_*() functions and log errors in their wrappers), so
keep that change and just simplify the do_check_device_state() function.

Fixes: 4309a3d38006395e797b86aac (“Make migration errors visible”)
Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

src/main.rs
@@ -591,7 +591,7 @@ impl<F: FileSystem + SerializableFileSystem + Send + Sync + 'static> VhostUserBa
         // Our caller (vhost-user-backend crate) pretty much ignores error objects we return (only
         // cares whether we succeed or not), so log errors here
         if let Err(err) = self.do_check_device_state() {
-            error!("Failed to conclude migration: {err}");
+            error!("Migration failed: {err}");
             return Err(err);
         }
         Ok(())
@@ -671,28 +671,18 @@ impl<F: FileSystem + SerializableFileSystem + Send + Sync + 'static> VhostUserFs
     }
 
     fn do_check_device_state(&self) -> io::Result<()> {
-        let result = if let Some(migration_thread) = self.migration_thread.lock().unwrap().take() {
-            // `Result::flatten()` is not stable yet, so no `.join().map_err(...).flatten()`
-            match migration_thread.join() {
-                Ok(x) => x,
-                Err(_) => Err(other_io_error("Failed to join the migration thread")),
-            }
-        } else {
+        let Some(migration_thread) = self.migration_thread.lock().unwrap().take() else {
             // `check_device_state()` must follow a successful `set_device_state_fd()`, so this is
             // a protocol violation
-            Err(io::Error::new(
+            return Err(io::Error::new(
                 io::ErrorKind::InvalidInput,
                 "Front-end attempts to check migration state, but no migration has been done",
-            ))
+            ));
         };
 
-        // Note that just like any other vhost-user message implementation, the error object that
-        // we return is not forwarded to the front end (it only receives an error flag), so if we
-        // want users to see some diagnostics, we have to print them ourselves
-        if let Err(e) = &result {
-            error!("Migration failed: {e}");
-        }
-        result
+        migration_thread
+            .join()
+            .map_err(|_| other_io_error("Failed to join the migration thread"))?
     }
 }
 